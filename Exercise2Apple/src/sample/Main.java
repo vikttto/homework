febaf6aeee99ca3551d630e5a7772278c99abced
Viktoria <104269255+vikttto@users.noreply.github.com>
2023-04-02 15:54:07 +0200

    Add files via upload

---
+++ Exercise2Apple/src/sample/Main.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int  appleCost;
+		appleCost = 2;
+		int appleBuy;
+		appleBuy = 6;
+		double finalCost;
+        finalCost = (appleCost * appleBuy) ;
+        System.out.println("Сума UAH " + finalCost);
+	}
+
+}
